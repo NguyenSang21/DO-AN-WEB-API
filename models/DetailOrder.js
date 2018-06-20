7d26563e148295320c9ec034a9ec168b0667a8e5
Nguyễn Sang <nguyentansang1996@gmail.com>
2018-06-20 22:21:05 +0700

    update hoadon

--- models/DetailOrder.js
+++ models/DetailOrder.js
@@ -15,12 +15,12 @@ exports.create = function (value ,callback) {
 	for(let i=0; i<value.length; i++)
 	{
 		values.push([value[i].idCTHD, value[i].idHD, value[i].idM, value[i].tenSanPham, value[i].SoLuong, value[i].DonGia, value[i].TongTien1SP, value[i].TrangThai]);
-		query += "Call updateQuantityMay("+value[i].idM+", "+value[i].SoLuong+");";		
+		query += "Call updateQuantityMay("+value[i].idM+", "+value[i].SoLuong+");";	
 	}
-	
-	query += "INSERT INTO chitiethd values ?";
+	db.executeQuery(query);
+	var query1 = "INSERT INTO chitiethd values ?";
 	console.log(query);
-	db.executeParamsQuery(query, [values], function (err, data){
+	db.executeParamsQuery(query1, [values], function (err, data){
         callback(err, data);
 	});
 	console.log("ok");
